2728dd047ee1ec1c96e707598f8954126b179e59
Tirth K. Sapariya <153990515+9510594131@users.noreply.github.com>
2025-01-04 19:35:28 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static('public'));  
+
+let todos = [];
+
+app.get('/api/todos', (req, res) => {
+  res.json(todos);
+});
+
+app.post('/api/todos', (req, res) => {
+  const todo = req.body;
+  todos.push(todo);
+  res.status(201).json(todo);
+});
+
+app.delete('/api/todos/:id', (req, res) => {
+  todos = todos.filter(todo => todo.id !== parseInt(req.params.id));
+  res.status(200).send('Todo deleted');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
